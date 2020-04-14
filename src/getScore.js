96e6b834beb9ac540b330aedd3028bc59630df89
mattia richetto <mattia.richetto@gmail.com>
2020-04-13 19:23:02 -0700

    simplify inner reduce

--- src/getScore.js
+++ src/getScore.js
@@ -11,12 +11,8 @@ const data = [
 function getScore(cells) {
   return cells.reduce((score, row) => {
     // eslint-disable-next-line no-param-reassign, no-return-assign
-    return (score += row.reduce((rowScore, current) => {
-      if (current.checked) {
-        // eslint-disable-next-line no-param-reassign
-        rowScore += 1;
-      }
-      return rowScore;
+    return (score += row.reduce((accumulator, current) => {
+      return current.checked ? accumulator + 1 : accumulator;
     }, 0));
   }, 0);
 }
