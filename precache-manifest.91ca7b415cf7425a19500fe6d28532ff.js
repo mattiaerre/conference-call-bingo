a065801b82f5cf8172ac9623d4eef25656c15463
mattia richetto <mattia.richetto@gmail.com>
2020-04-14 10:37:55 -0700

    Updates

---
+++ precache-manifest.91ca7b415cf7425a19500fe6d28532ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d26979d50557669103f0bc1e4d264a",
+    "url": "/conference-call-bingo/index.html"
+  },
+  {
+    "revision": "10dcb9bdcd3b1914a3cc",
+    "url": "/conference-call-bingo/static/css/main.82a88149.chunk.css"
+  },
+  {
+    "revision": "8012464a47988f4262f2",
+    "url": "/conference-call-bingo/static/js/2.57d030fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/conference-call-bingo/static/js/2.57d030fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10dcb9bdcd3b1914a3cc",
+    "url": "/conference-call-bingo/static/js/main.39d43905.chunk.js"
+  },
+  {
+    "revision": "1339bc01241bcdaf100b",
+    "url": "/conference-call-bingo/static/js/runtime-main.dae88842.js"
+  }
+]);
