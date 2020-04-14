f180c329d7869165de8dc9ef250cef55e7be4025
mattia richetto <mattia.richetto@gmail.com>
2020-04-14 09:21:47 -0700

    even more functional

--- src/getScore.js
+++ src/getScore.js
@@ -38,12 +38,14 @@ const data = [
 ];
 */
 
-function getScore(cells) {
-  return cells.reduce((score, row) => {
-    // eslint-disable-next-line no-param-reassign, no-return-assign
-    return (score += row.reduce((accumulator, current) => {
-      return current.checked ? accumulator + 1 : accumulator;
-    }, 0));
+function getScore(marks) {
+  return marks.reduce((score, row) => {
+    return (
+      score +
+      row.reduce((counter, mark) => {
+        return counter + (mark.checked ? 1 : 0);
+      }, 0)
+    );
   }, 0);
 }
 
